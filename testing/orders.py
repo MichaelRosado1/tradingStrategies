a2cf62256f68d45e66c9a86add06b32ae37be431
MichaelRosado1 <mrosad4@illinois.edu>
2021-07-23 20:41:08 -0500

    method implemented to check the total buying power in the account

--- testing/orders.py
+++ testing/orders.py
@@ -1,6 +1,5 @@
 import alpaca_trade_api as tradeapi
 import os
-from alpaca_trade_api.entity import Position
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,13 +12,28 @@ class Trader:
 		self.key_id = os.getenv('API_KEY')
 		self.secret_key = os.getenv('SECRET_KEY')
 		self.base_url = 'https://paper-api.alpaca.markets'
+		self.symbol = symbol
+		self.order_size = orderSize
+		self.api = tradeapi.REST(
+			self.key_id, 
+			self.secret_key, 
+			self.base_url
+		)
+
+	def check_account_details(self):
+		account = self.api.get_account()
+		print('${} is available as buying power.'.format(account.buying_power))
+
+
 
 def main():
-	print('hello, world!')
+	trader = Trader('AAPL', 10)
+	trader.check_account_details()
+
 
 
 
 
 if __name__ == '__main__':
+
 	main()
-	t = Trader()
