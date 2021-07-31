cbd2ee0a5b3aca3aa08c30b326d647f1fae5661d
MichaelRosado1 <mrosad4@illinois.edu>
2021-07-31 11:07:56 -0500

    intial file setup

---
+++ meanRev/meanr.py
@@ -0,0 +1,28 @@
+'''
+    Mean reversion strategy assumes that the price volitility and historical returns will revert to teh
+    long run mean or average
+
+
+'''
+
+#trade library
+import alpaca_trade_api as tradeapi
+
+#libraries to get env vars
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+#main function will handle trade initialization
+def main():
+    print('')
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
