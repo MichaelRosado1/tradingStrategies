1918557bd9c29a5b19f58d85cbc2fd178039c1db
MichaelRosado1 <mrosad4@illinois.edu>
2021-08-05 09:27:03 -0500

    refactored mean_reversion.py and added file to handle all data stream functions

--- meanRev/mean_reversion.py
+++ meanRev/mean_reversion.py
@@ -4,6 +4,8 @@ The mean_reversion file will contain all of the trading logic required for the m
 
 
 '''
+import websocket
+import json
 class mean_reversion:
     def __init__(self, trader):
         #this will be the trader object that contains all of the buy/sell functions for the api
@@ -25,15 +27,5 @@ class mean_reversion:
         self.moving_average = ave
         return ave
 
-    def start_trading(self):
-        conn = self.trader.__api.stream2.StreamConn(self.trader.__key_id, self.trader.__secret_key, self.trader.__base_url)
-
-        @conn.on(r'^account_updates$')
-        async def on_account_updates(conn, channel, account):
-            print('account', account)
-
-        @conn.on(r'^trade_updates$')
-        async def on_trade_updates(conn, channel, trade):
-            print('trade', trade)
 
 
