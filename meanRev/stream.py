4aa6f41108d8227388e7b29ea9077f46ef9a19e0
MichaelRosado1 <mrosad4@illinois.edu>
2021-08-29 21:24:25 -0500

    starting to refactor stream file into a stream class to be able to communicate with the trading logic directly

--- meanRev/stream.py
+++ meanRev/stream.py
@@ -6,37 +6,41 @@ import websocket
 import os
 from dotenv import load_dotenv
 
-
-def on_open(ws):
-    print('opened conection')
-    auth_data = {
-        'action': 'authenticate',
-        'data': {
-                'key_id': os.getenv('API_KEY'),
-                'secret_key': os.getenv('SECRET_KEY')
-            }
-    }
-
-    ws.send(json.dumps(auth_data))
-
-    listen_message = {'action': 'listen', 'data': {'streams': ['minute.SPY']}}
-
-    ws.send(json.dumps(listen_message))
+class stream:
+    def __init__(self):
+        connect()
 
 
-def on_message(ws, message):
-    #I need to refactor the trade logic class in order to allow this file to communicate
-    #with is often
-    print(message)
+    def on_open(ws):
+        print('opened conection')
+        auth_data = {
+            'action': 'authenticate',
+            'data': {
+                    'key_id': os.getenv('API_KEY'),
+                    'secret_key': os.getenv('SECRET_KEY')
+                }
+        }
+
+        ws.send(json.dumps(auth_data))
+
+        listen_message = {'action': 'listen', 'data': {'streams': ['minute.SPY']}}
+
+        ws.send(json.dumps(listen_message))
 
 
-def on_close(ws):
-    print('closed connection')
+    def on_message(ws, message):
+        #we don't need the entire message, we just need the price data so we can know
+        #when to make trades 
+        
 
 
-def connect():
-    socket = 'wss://data.alpaca.markets/stream'
-    
-    ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
-    ws.run_forever()
+    def on_close(ws):
+        print('closed connection')
+
+
+    def connect():
+        socket = 'wss://data.alpaca.markets/stream'
+        
+        ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
+        ws.run_forever()
 
